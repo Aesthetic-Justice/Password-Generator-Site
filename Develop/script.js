0b9b38bbfcef3096cae9578a6abec159567921eb
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2022-08-31 07:22:52 -0400

    BugFix: Check for character options included

--- Develop/script.js
+++ Develop/script.js
@@ -11,7 +11,7 @@ function generatePassword() {
   choices2D[0] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   //Uppercase characters, index 1
   choices2D[1] = [];
-  for(let i=0;i<choices2D[0].length;i++){
+  for (let i = 0; i < choices2D[0].length; i++) {
     choices2D[1].push(choices2D[0][i].toUpperCase());
   };
   //Numerics, index 2
@@ -23,42 +23,49 @@ function generatePassword() {
   const choices = [0, 0, 0, 0];
 
   //Confirm the 4 choices
-  let confirmchoice = confirm("Do you want lowercase characters in your password?");
-  if(confirmchoice){
-    choices[0]=1;
-  }
+  let confirmchoice;
+  do {
+    confirmchoice = confirm("Do you want lowercase characters in your password?");
+    if (confirmchoice) {
+      choices[0] = 1;
+    };
 
-  confirmchoice = confirm("Do you want uppercase characters in your password?");
-  if(confirmchoice){
-    choices[1]=1;
-  }
+    confirmchoice = confirm("Do you want uppercase characters in your password?");
+    if (confirmchoice) {
+      choices[1] = 1;
+    };
 
-  confirmchoice = confirm("Do you want numeric characters in your password?");
-  if(confirmchoice){
-    choices[2]=1;
-  }
+    confirmchoice = confirm("Do you want numeric characters in your password?");
+    if (confirmchoice) {
+      choices[2] = 1;
+    };
 
-  confirmchoice = confirm("Do you want special characters in your password?");
-  if(confirmchoice){
-    choices[3]=1;
-  }
+    confirmchoice = confirm("Do you want special characters in your password?");
+    if (confirmchoice) {
+      choices[3] = 1;
+    };
+    if(choices[0] === 0 && choices[1] === 0 && choices[2] === 0 && choices[3] === 0){
+      window.alert("Invalid Entry: A password cannot be generated if all character options are declined.");
+      continue;
+    };
+  } while (choices[0] === 0 && choices[1] === 0 && choices[2] === 0 && choices[3] === 0);
 
   //the total number of potential characters the password could contain
   const choicesTotalVal = (choices[0] * 26) + (choices[1] * 26) + (choices[2] * 10) + (choices[3] * 33);
   const choicesTotal = [];
 
   //a for loop that iterates through all potential characters
-  for(let i=0;i<choices2D.length;i++){
+  for (let i = 0; i < choices2D.length; i++) {
     //if the index in question is default(0), skip
-    if(choices[i]===0){
+    if (choices[i] === 0) {
       continue;
-    }
+    };
 
     //else, add them to total choices
-    for(let i2=0;i2<choices2D[i].length;i2++){
+    for (let i2 = 0; i2 < choices2D[i].length; i2++) {
       choicesTotal.push(choices2D[i][i2]);
-    }
-  }
+    };
+  };
 
   //the password, which this password will return
   let password = [];
@@ -67,26 +74,28 @@ function generatePassword() {
   let passwordLength;
 
   //Loop to ensure the password entry is valid
-  do{
+  do {
     //prompt the user for the length of the password
     passwordLength = window.prompt("What length do you want the password to be?(min.8/max.128");
-    
-    //If the entered value is Not a Number, set it to 0, which fails the invalid below
-    if(isNaN(passwordLength)){
-      window.alert("Invalid Entry: Not a Number.\nPassword length must be between 8 and 128.")
-    }
 
-    //if passowrd is out of bounds, alert the user
-    if(passwordLength<8||passwordLength>128){
+    //If the entered value is Not a Number, alert the user, then reset
+    if (isNaN(passwordLength)) {
+      window.alert("Invalid Entry: Not a Number.\nPassword length must be between 8 and 128.")
+      continue;
+    };
+
+    //if passowrd is out of bounds, alert the user, then reset
+    if (passwordLength < 8 || passwordLength > 128) {
       window.alert("Invalid Entry: Out of Bounds.\nPassword length must be between 8 and 128.");
-    }
+      continue;
+    };
     //loop password input until input is both a Number, and within bounds
-  }while(passwordLength<8||passwordLength>128);
+  } while (passwordLength < 8 || passwordLength > 128||isNaN(passwordLength)==true);
 
   //the main loop which generates password char by char
   for (i = 0; i < passwordLength; i++) {
     password.push(choicesTotal[Math.floor(Math.random() * choicesTotal.length)]);
-  }
+  };
 
   //returns the result
   return password.join("");
